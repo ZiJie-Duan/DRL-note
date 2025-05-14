26af430ff13345cc10611287e23e06b1dce63da6
Zijie Duan <zddua@student.unimelb.edu.au>
2025-05-14 16:19:42 +1000

    .

--- ox_chess_Q/test.py
+++ ox_chess_Q/test.py
@@ -1,11 +1,33 @@
-from game import OXGame
 from game2 import OXGame2
 
+import time
+
 ox = OXGame2()
-ox.pprint(*ox.o_move(1))
-ox.pprint(*ox.x_move(5))
-ox.pprint(*ox.o_move(4))
-ox.pprint(*ox.x_move(7))
-ox.pprint(*ox.o_move(2))
-ox.pprint(*ox.x_move(9))
+
+start_time = time.time()
+ox.o_move(1)
+print(f"ox.o_move(1) took {time.time() - start_time} seconds")
+
+start_time = time.time()
+ox.x_move(5)
+print(f"ox.x_move(5) took {time.time() - start_time} seconds")
+
+start_time = time.time()
+ox.o_move(4)
+print(f"ox.o_move(4) took {time.time() - start_time} seconds")
+
+start_time = time.time()
+ox.x_move(7)
+print(f"ox.x_move(7) took {time.time() - start_time} seconds")
+
+start_time = time.time()
+ox.o_move(2)
+print(f"ox.o_move(2) took {time.time() - start_time} seconds")
+
+start_time = time.time()
+ox.x_move(9)
+print(f"ox.x_move(9) took {time.time() - start_time} seconds")
+
+start_time = time.time()
 print(ox.x_move(3))
+print(f"ox.x_move(3) took {time.time() - start_time} seconds")
